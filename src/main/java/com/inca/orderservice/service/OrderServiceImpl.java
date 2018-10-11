26801f29ceb916423e197abf275a62067da07b41
史战伟 <shizhanwei123@126.com>
2018-10-12 07:42:45 +0800

    ribbon调用服务第二种方式

--- src/main/java/com/inca/orderservice/service/OrderServiceImpl.java
+++ src/main/java/com/inca/orderservice/service/OrderServiceImpl.java
@@ -3,6 +3,9 @@ package com.inca.orderservice.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inca.orderservice.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,12 +14,20 @@ import java.util.HashMap;
 @Service(OrderService.name)
 public class OrderServiceImpl implements OrderService {
 
+//    @Autowired
+//    RestTemplate restTemplate;
+
     @Autowired
-    RestTemplate restTemplate;
+    LoadBalancerClient loadBalancerClient;
 
     @Override
     public Order saveOrder(Order order) throws Exception {
-        Object obj = restTemplate.getForObject("http://product-service/api/product/findOne?id=" + order.getProductId(), Object.class);
+//        Object obj = restTemplate.getForObject("http://product-service/api/product/findOne?id=" + order.getProductId(), Object.class);
+
+        ServiceInstance instance = loadBalancerClient.choose("product-service");
+        String url=String.format("http://%s:%s/api/product/findOne?id=" + order.getProductId(),instance.getHost(),instance.getPort());
+        RestTemplate restTemplate=new RestTemplate();
+        Object obj = restTemplate.getForObject(url, Object.class);
         ObjectMapper om = new ObjectMapper();
         String skuStr = om.writeValueAsString(obj);
         HashMap sku = om.readValue(skuStr, HashMap.class);
